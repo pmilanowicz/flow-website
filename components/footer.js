94a70899f0353f7514fccd212bd630ea9069fd84
Paweł Milanowicz <pawmilanowicz@gmail.com>
2022-04-09 22:44:39 +0200

    fix: change styling

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,4 @@
 import { Grid, List, Link, ListItem, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import SocialsBarComponent from "./socials-bar";
 import FlowLogo from '../public/images/flow.png';
 import Image from 'next/image';
@@ -8,22 +7,12 @@ import { Box } from "@mui/system";
 import { MenuElements } from "../types/menu-elements";
 import { emailAddress } from "../types";
 
-const useStyles = makeStyles((theme) => ({
-    logoBox: {
-        marginTop: -40,
-        [theme.breakpoints.down('sm')]: {
-            display: "none"
-        }
-    }
-}));
-
 export default function Footer() {
-    const classes = useStyles();
 
     return (
         <Grid container padding={0}>
             <Grid item xs={12} textAlign="center" sx={{ mt: 5 }}>
-                <Box className={classes.logoBox}>
+                <Box sx={{ display: { sm: "none", md: "block" } }}>
                     <NextLink href="/">
                         <a>
                             <Image src={FlowLogo} width="150" height="150" />
--- components/main-bar.js
+++ components/main-bar.js
@@ -1,33 +1,19 @@
 import { Button, Grid, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-
-const useStyles = makeStyles((theme) => ({
-  gridContainer: {
-    backgroundColor: theme.palette.primary.main,
-    marginTop: 20,
-    paddingTop: 15,
-    paddingBottom: 15
-  },
-}));
 
 export default function MainBar() {
-  const classes = useStyles();
-
   return (
-    <Grid container>
-      <Grid container direction="row" justifyItems="center" justifyContent="center" className={classes.gridContainer}>
-        <Grid item xs={12} padding={4} textAlign="center">
-          <Typography variant="h4" color="secondary">
-            Rekrutujemy!
-          </Typography>
-          <Typography variant="h5" color="secondary">
-            Zostań zawodnikiem lub zawodniczką Flow! <br/>
-            Więcej informacji na wydarzeniu<br/><br />
-            <Button size="small" variant="contained" color="secondary"
-                    href="https://www.facebook.com/events/468306045087768/471653434753029" target="_blank">Kliknij i dołącz
-            </Button>
-          </Typography>
-        </Grid>
+    <Grid container direction="row" justifyItems="center" justifyContent="center"  backgroundColor="#1e3356" marginTop={2} paddingY={1}>
+      <Grid item xs={12} padding={4} textAlign="center">
+        <Typography variant="h4" color="secondary">
+          Rekrutujemy!
+        </Typography>
+        <Typography variant="h5" color="secondary">
+          Zostań zawodnikiem lub zawodniczką Flow! <br />
+          Więcej informacji na wydarzeniu<br /><br />
+          <Button size="small" variant="contained" color="secondary"
+            href="https://www.facebook.com/events/468306045087768/471653434753029" target="_blank">Kliknij i dołącz
+          </Button>
+        </Typography>
       </Grid>
     </Grid>
   )
